2ea550907ffb05297e96fc35b1d3d96408dccba4
Laura Ebber <lauraebber@gmail.com>
2023-05-17 18:22:13 +0300

    Create a new app

---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userchoise = Console.ReadLine();
+
+if (userchoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    printgoodbye();
+}
+
+
+ 
+
+
+
+static void PrintHello() //Function ehk meetod
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void printgoodbye()
+{
+    Console.WriteLine("See you later, aligator!");
+}
